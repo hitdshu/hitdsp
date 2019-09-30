63bf7b0a545491409fb417d8708d88199c5a7d1d
hudeshun <dshu@nullmax.ai>
2019-09-30 13:09:49 +0800

    Add ldpc encoder and testcase.

--- ecc/ldpc.cc
+++ ecc/ldpc.cc
@@ -1,3 +1,4 @@
+#include <cassert>
 #include "ecc/ldpc.h"
 
 namespace hitdsp {
@@ -13,29 +14,69 @@ void Ldpc::Init(const ::std::vector<::std::vector<int>> &base_graph, int z) {
     row_weights_ = ::std::vector<int>(m_, 0);
     row_mat_.resize(m_);
     col_mat_.resize(n_);
+    int index = 0;
     for (size_t ridx = 0; ridx < base_graph.size(); ++ridx) {
         const ::std::vector<int> &row_tmp = base_graph[ridx];
-        for (size_t cidx = 0; cidx < row_tmp.size(); ++cidx) {
+        assert(n_ == row_tmp.size());
+        for (size_t cidx = 0; cidx < n_; ++cidx) {
             if (row_tmp[cidx] != -1) {
                 col_weights_[cidx]++;
                 row_weights_[ridx]++;
-                row_mat_[ridx].push_back(cidx);
-                col_mat_[cidx].push_back(ridx);
+                row_mat_[ridx].push_back(index);
+                col_mat_[cidx].push_back(index);
             }
+            index++;
         }
     }
 }
 
 void Ldpc::Encode(const uint8_t *data, uint8_t *symbols, int data_bits) {
-
+    for (int idx = 0; idx < k_ * z_; ++idx) {
+        symbols[idx] = data[idx];
+    }
+    ::std::vector<::std::vector<uint8_t>> parity(m_, ::std::vector<uint8_t>(z_, 0));
+    ::std::vector<uint8_t> tmp(z_, 0);
+    for (int ridx = 0; ridx < m_; ++ridx) {
+        for (int ccidx = 0; ccidx < row_weights_[ridx]; ++ccidx) {
+            int index = row_mat_[ridx][ccidx];
+            int row = index / n_;
+            int col = index % n_;
+            if (col >= k_) {
+                break;
+            }
+            MulSh(&symbols[col * z_], &tmp[0], base_graph_[row][col]);
+            for (int pidx = 0; pidx < z_; ++pidx) {
+                parity[ridx][pidx] ^= tmp[pidx];
+            }
+        }
+    }
+    for (int idx1 = 0; idx1 < z_; ++idx1) {
+        tmp[idx1] = 0;
+        for (int idx2 = 0; idx2 < m_; ++idx2) {
+            tmp[idx1] ^= parity[idx2][idx1];
+        }
+        symbols[k_ * z_ + idx1] = tmp[idx1];
+    }
+    for (int ridx = 0; ridx < m_; ++ridx) {
+        MulSh(&symbols[k_ * z_], &tmp[0], base_graph_[ridx][k_]);
+        for (int pidx = 0; pidx < z_; ++pidx) {
+            parity[ridx][pidx] ^= tmp[pidx];
+        }
+    }
+    ::std::vector<uint8_t> prevp(z_, 0);
+    int index = (k_ + 1) * z_;
+    for (int idx = 0; idx < m_ - 1; ++idx) {
+        for (int pidx = 0; pidx < z_; ++pidx) {
+            prevp[pidx] ^= parity[idx][pidx];
+            symbols[index++] = prevp[pidx];
+        }
+    }
 }
 
 void Ldpc::Decode(const uint8_t *symbols, uint8_t *data, int data_bits) {
-
 }
 
 void Ldpc::Decode(const float *symbols, uint8_t *data, int data_bits) {
-
 }
 
 bool Ldpc::CheckLdpc(const uint8_t *cw) const {
@@ -52,10 +93,9 @@ bool Ldpc::CheckLdpc(const uint8_t *cw) const {
     for (auto &tmp : syn) {
         if (tmp != 0) {
             return false;
-        } else {
-            return true;
         }
     }
+    return true;
 }
 
 void Ldpc::MulSh(const uint8_t *input, uint8_t *output, int k) const {
@@ -66,10 +106,10 @@ void Ldpc::MulSh(const uint8_t *input, uint8_t *output, int k) const {
     } else {
         int index = 0;
         for (int idx = k; idx < z_; ++idx) {
-            output[index++] = output[idx];
+            output[index++] = input[idx];
         }
         for (int idx = 0; idx < k; ++idx) {
-            output[index++] = output[idx];
+            output[index++] = input[idx];
         }
     }
 }
